04ec17c97c90d9ecd4d35fc40644cc3b87e2283a
mehraasahil <mehra3249@gmail.com>
2023-03-07 02:38:53 +0530

    added

---
+++ index.js
@@ -0,0 +1,55 @@
+
+//  1 .function example
+// function sahil(){
+//     console.log('sahil')
+    
+// }
+// sahil();
+
+
+// 2. function with parameter 
+
+// function data(name,age){
+//     console.log('hello' + name + 'my age is '+ age)
+// }
+
+// data('sahil','23');
+
+
+// 3. function with return
+
+// function sum(math,sci,eng){
+//     var s = math+sci+eng
+//     return s;
+// }
+
+
+
+// function percentage(tt){
+//     var per = tt/300*100;
+//    console.log(per)
+// }
+// var total = sum(15,23,44);
+// percentage(total)
+
+
+
+
+
+
+
+
+
+
+
+
+// const sah=[{name:'sahil'}];
+
+// function df(x){
+//     return x.name;
+// }
+
+// const out = sah.map(df)
+// console.log(out)
+
+
